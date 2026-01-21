4d8522d1d3e2beb5d60b5e51969388018506af1c
Archit Sharma <151552346+Archit1704@users.noreply.github.com>
2026-01-21 22:35:30 +0530

    Update check-file.sh

--- check-file.sh
+++ check-file.sh
@@ -3,17 +3,16 @@
 file="$1"
 
 if [ -z "$file" ]; then
-    echo "Usage: $0 filename"
-    exit 1
+echo "Usage: $0 filename"
+exit 1
 fi
-
 if [ -e "$file" ]; then
-    if [ -r "$file" ]; then
-        cat "$file"
-    else
-        echo "Permission denied: Cannot read '$file'"
-    fi
+if [ -r "$file" ]; then
+cat "$file"
 else
-    echo "File not found"
+echo "Permission denied: Cannot read '$file'"
+fi
+else
+echo "File not found"
 fi
 
